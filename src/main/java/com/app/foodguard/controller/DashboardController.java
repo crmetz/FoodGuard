e1d03460870a7faa858f83694e58f37398eb2a14
crmetz <cristianrmetz@gmail.com>
2025-05-04 23:28:51 -0300

    feat: add button to return to dashboard

--- src/main/java/com/app/foodguard/controller/DashboardController.java
+++ src/main/java/com/app/foodguard/controller/DashboardController.java
@@ -1,46 +1,40 @@
 package com.app.foodguard.controller;
 
-import com.app.foodguard.utils.IconManager;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+
 import java.io.IOException;
 
 public class DashboardController {
 
     @FXML private StackPane dynamicContent;
-    @FXML private Label greetingLabel;
-    @FXML private Button notificationButton;
-    @FXML private Button profileButton;
 
     @FXML
     public void initialize() {
-        // Exemplo: Carregar nome do usuário (substitua com lógica real)
-        greetingLabel.setText("Olá, " + "Nome do Usuário");
-        setupIcons();
+        loadDashboardContent();
+
     }
 
-    private void setupIcons() {
-        // Notification bell icon
-        notificationButton.setGraphic(
-                IconManager.getIcon(FontAwesomeIcon.BELL, "#2c3e50", 1.5)
-        );
-
-        // Profile user icon (in the sidebar)
-        profileButton.setGraphic(
-                IconManager.getIcon(FontAwesomeIcon.USER, "white", 1.2)
-        );
-        profileButton.setText("Perfil"); // Remove text if you only want the icon
-
+    @FXML
+    private void loadDashboardContent() {
+        try {
+            Parent dashboardView = FXMLLoader.load(getClass().getResource("/com/app/foodguard/dashboard/dashboard-content-view.fxml"));
+            dynamicContent.getChildren().setAll(dashboardView);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // Métodos para carregar as telas
     @FXML
     private void loadAlimentos() {
+        loadView("/com/app/foodguard/alimento/alimento-view.fxml");
+    }
+
+    @FXML
+    private void loadCategorias() {
         loadView("/com/app/foodguard/alimento/alimento-view.fxml");
     }
 
@@ -74,7 +68,6 @@ public class DashboardController {
         loadView("/com/app/foodguard/Estoque/estoque-view.fxml");
     }
 
-
     private void loadView(String fxmlPath) {
         try {
             Parent view = FXMLLoader.load(getClass().getResource(fxmlPath));
@@ -88,4 +81,5 @@ public class DashboardController {
     private void loadPerfil() {
         // Implemente a lógica para carregar a tela de perfil
     }
+
 }
