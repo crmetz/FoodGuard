fd360ef0e524a3af17cd420da3bd12e89ded0167
Mauricio <mauricio.bono.mbn@gmail.com>
2025-06-27 21:18:32 -0300

    Sistema de notificações

--- src/main/java/com/app/foodguard/controller/DashboardController.java
+++ src/main/java/com/app/foodguard/controller/DashboardController.java
@@ -47,4 +47,58 @@ public class DashboardController {
             this.lotesPendentes = lotes;
         }
     }
+    // Métodos para carregar as telas
+    @FXML
+    private void loadAlimentos() {
+        loadView("/com/app/foodguard/alimento/alimento-view.fxml");
+    }
+
+    @FXML
+    private void loadCategorias() {
+        loadView("/com/app/foodguard/categoria/categoria-view.fxml");
+    }
+
+    @FXML
+    private void loadEstoque() {
+        loadView("/com/app/foodguard/lote/lote-view.fxml");
+    }
+
+    @FXML
+    private void loadReceita() {
+        loadView("/com/app/foodguard/receita/receita-view.fxml");
+    }
+
+    @FXML
+    private void loadDesperdicios() {
+        loadView("/com/app/foodguard/desperdicio/desperdicio-view.fxml");
+    }
+
+    @FXML
+    private void loadDoacoes() {
+        loadView("/com/app/foodguard/Estoque/estoque-view.fxml");
+    }
+
+    @FXML
+    private void loadOngs() {
+        loadView("/com/app/foodguard/ongs/ongs-view.fxml");
+    }
+
+    @FXML
+    private void loadRelatorios() {
+        loadView("/com/app/foodguard/Estoque/estoque-view.fxml");
+    }
+
+    private void loadView(String fxmlPath) {
+        try {
+            Parent view = FXMLLoader.load(getClass().getResource(fxmlPath));
+            dynamicContent.getChildren().setAll(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void loadPerfil() {
+        // Implemente a lógica para carregar a tela de perfil
+    }
 }
